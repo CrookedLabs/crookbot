69c5203631116ffae532871cea2f7cef34783ff8
Chris Sciolla <csciolla1@gmail.com>
2016-02-19 11:22:08 -0500

    update some stuffs

--- bot.js
+++ bot.js
@@ -51,4 +51,3 @@ http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Ok, dyno is awake.');
 }).listen(process.env.PORT || 5000);
-
